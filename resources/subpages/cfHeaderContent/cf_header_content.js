cf36d98210dcf670b70db0103ab715025e8aa1b3
Gyuszi <gyhrubi@comforth.hu>
2017-08-10 14:23:21 +0200

    Tartományi bejelentkezés, bejelentkezés után kijeletkezési lehetőség megjelenítése, logout eljárás visszaintegrálása.

--- resources/subpages/cfHeaderContent/cf_header_content.js
+++ resources/subpages/cfHeaderContent/cf_header_content.js
@@ -10,26 +10,5 @@ app.controller('cfHeaderContentCtrl', ['$scope','$rootScope','$location','Webapi
           
     }
     
-    // Logout function:
-    $scope.logout = function (fnCallback) {
-            
-            $scope.logoutError = false;
-            var promise = Webapi.logout();
-            promise.success(function (response) {
-                $rootScope.initUser();
-                $rootScope.setUserSession();
-            })
-            promise.error(function (err, status) {
-                $rootScope.initUser();
-                $scope.logoutError = true;
-                $rootScope.setUserSession();
-            });
-
-            //$route.reload();
-
-            if (fnCallback) {
-                fnCallback();
-            }
-    }
     
 }]);
